fb89b795d6328629d2de6f6df316209a3327d9ac
Lis Kenny <liskenny@lis-kennys-imac.home>
2014-08-18 21:02:33 +0100

    Initial Commit

---
+++ Swift Launch Test1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Launch Test1
+//
+//  Created by Lis Kenny on 18/08/2014.
+//  Copyright (c) 2014 Lis Kenny. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
